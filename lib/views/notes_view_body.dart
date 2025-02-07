9efa17dbdaeac9bdc2691c4f52ee2eb7b96d8995
Moataz303 <118053254+Moataz303@users.noreply.github.com>
2025-02-07 18:25:09 +0200

    create NotesListView

--- lib/views/notes_view_body.dart
+++ lib/views/notes_view_body.dart
@@ -15,11 +15,11 @@ class NotesViewBody extends StatelessWidget {
             height: 50,
           ),
           CustomAppBar(),
-          Expanded(child: NotesListView()),
+          Expanded(
+            child: NotesListView(),
+          ),
         ],
       ),
     );
   }
 }
-
-
